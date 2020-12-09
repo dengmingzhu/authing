525da67a876de81b33126d8416d9972c73f2ea03
kaka <wuwanggao@163.com>
2020-12-09 22:41:01 +0800

    init  project

---
+++ src/main/java/com/dmz/authing/AuthingApplication.java
@@ -0,0 +1,13 @@
+package com.dmz.authing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dmz/authing/AuthingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmz.authing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
